2fb53fee31ae43bae2adbe8ecc838203a43a7e39
omniintelligence <66555624+omniintelligence@users.noreply.github.com>
2022-06-14 04:58:32 +0530

    '9gag'

---
+++ public/js/seed.js
@@ -0,0 +1,46 @@
+window.Seed = (function () {
+  function generateVoteCount() {
+    return Math.floor((Math.random() * 50) + 15);
+  }
+
+  const products = [
+    {
+      id:1,
+      title: 'Yellow Paul', 
+      description: 'On-demand sand castle construction expertise.',
+      url:'#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: 'images/Avatars/daniel.jpg',
+      productImageUrl: 'images/products/scarlettjohansson.jpg',
+    },
+    {
+      id:2,
+      title: 'Supermajority: The Fantasy Congress League', 
+      description: 'Earn points when your favourite politicians pass legislation.',
+      url:'#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: 'images/Avatars/kristy.png',
+      productImageUrl: 'images/products/sc.jpg',
+    },
+    {
+      id:3,
+      title: 'Tinfoild: Tailored tinfoil hats', 
+      description: 'We already have your measurements and shipping address.',
+      url:'#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: 'images/Avatars/veronika.jpg',
+      productImageUrl: 'images/products/sc2.jpg',
+    },
+    {
+      id:4,
+      title: 'Haught or Naught', 
+      description: 'High-minded or absent-minded? You decide.',
+      url:'#',
+      votes: generateVoteCount(),
+      submitterAvatarUrl: 'images/Avatars/molly.png',
+      productImageUrl: 'images/products/sc3.jpg',
+    },
+  ];
+
+  return {products: products};
+})();
