0f99f93062bb00ed84b7444e5af30ef0736f069c
Your Name <you@example.com>
2022-10-04 21:12:26 +0530

    launcing bicycle shop

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import AddProduct from "./components/addproduct";
+import Payment from "./components/payment";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
