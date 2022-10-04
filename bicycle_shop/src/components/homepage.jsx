50370c7c6dec8b6860e7e51ba8d5bb2e9d2db7a1
Your Name <you@example.com>
2022-10-04 20:46:30 +0530

    launching bicycle shop

---
+++ bicycle_shop/src/components/homepage.jsx
@@ -0,0 +1,11 @@
+ import Navbar from "./navbar";
+import Product from "./product";
+
+export default function HomePage() {
+    return(
+        <>
+        <Navbar/>
+        <Product/>
+        </>
+    );
+}
